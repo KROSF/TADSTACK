c03422e67af8739bf008d80f36e06530881f968e
KROSF <rodrigosanabria22@gmail.com>
2017-11-26 23:01:08 +0100

    exercise 3 without stack complete
    Signed-off-by: KROSF <rodrigosanabria22@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,64 +1,45 @@
 #include <iostream>
 #include "pila.h"
 using namespace std;
-bool equate(string);
-bool shape(string);
+void stck(Pila<int>&,int,int);
+int main()
+{
+    int vec[13]={3,4,1,3,4,1,3,4,2,4,2,2,3};
+    int a=1,b=2;
+    bool sts = true;
+    for(int i=0;i<13&&sts;i++)
+    {
+        if(vec[i]==a )
+        {
+            a=i;
+            sts = false;
 
-int main() {
-    string X="ABC&CBA";
-    string Y="AC&CA#CBA&ABC#ABC&CBA#CBDA&ADBC";
-    //cout<<equate(X)<<endl;
-    cout<<shape(Y)<<endl;
+            while(vec[i]!=b)
+             i++;
+
+            b=i;
+        }
+    }
+
+    for(int i=a;i<b;i++,b--)
+        swap(vec[i],vec[b]);
+
+    for (int i : vec)
+        cout<< i <<" ";
 
     return 0;
 }
-bool equate(const string X){
-    Pila <char> P1(X.length());
-
-    int i;
-
-    for(i=0;X[i]!='&';i++)
-        P1.push(X[i]);
-
-    for(i+=1;i<X.length()&&X[i]==P1.tope();i++)
-        P1.pop();
-
-    return (X.length()==i) ;
-}
-bool shape(const string X)
-{   unsigned int cont=0;
-    int k=0;
-    bool sts=true;
-    //Cuenta apariciones de #
-    for (char i : X) {
-        if(i =='#') cont++;
-    }
-    //Creamos pilas con el numero de # +1
-    Pila<string> P(cont+1);
-    //Metemos cada cadena en la pila
-    for(int j=0;j<cont;j++)
+void stck(Pila<int>& P,int a,int b){
+    Pila<int>AUX(19);
+    while(!P.vacia())
     {
-        string AUX;
-
-        while(X[k]!='#')
-        {
-            AUX+=X[k];
-            k++;
-        }
-        P.push(AUX);
-        k+=1;
+        cout<<P.tope()<<" ";
+        AUX.push(P.tope());
+        P.pop();
     }
-    //Debido a un fallo no se me ocurre otra forma de metar la ultima cadena a la pila
-    string AUX = X.substr (k);
-    P.push(AUX);
-    //Comprobamos si la pila es de la forma pedida
-    for(int i=0;i<cont+1 && sts;i++)
-    {
-        if(equate(P.tope()))
-            P.pop();
-
-        else sts = false;
+    cout<<endl;
+    while(!AUX.vacia()){
+        cout<<AUX.tope()<<" ";
+        AUX.pop();
     }
-//
-    return sts;
 }
