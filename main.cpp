2160d41d75ef8fdcf1bf1519e18fb153222f595c
rafaroman18 <33625416+rafaroman18@users.noreply.github.com>
2017-11-27 20:31:26 +0100

    Ejercicio 1 Forma Rafa, posiblemente encontrado el kit de la cuestion

--- main.cpp
+++ main.cpp
@@ -4,26 +4,43 @@ using namespace std;
 bool shuffle (const string );
 int main()
 {
-    string X="abc&ca";
-    cout<<shuffle(X);
+    string X="abc&cba";
+    if(shuffle(X))
+    {
+        cout<<"La cadena es de la forma X&Y"<<endl;
+    }
+    else
+        cout<<"La cadena NO es de la forma X&Y"<<endl;
     return 0;
 }
 bool shuffle (const string X)
 {
+    bool valido=true;
     int i;
     Pila <char>P(X.length());
-    for(i=0;i<X.length();i++)
+    for(i=0;X[i]!='&'&& i<X.length();i++)
     {
        P.push(X[i]);
+    }
+    if(P.llena())
+    {
+        valido = false;
+    }
+    else
+        {
+    P.push('&');
+    for(i=i+1;i<X.length();i++)
+    {
+        P.push(X[i]);
     }
     for(i=0;i<X.length();i++)
     {
         if(P.tope()!=X[i])
         {
-            cout<<"tu ere tonto"<<endl;
-            return false;
+            valido= false;
+            i=X.length();
         }
         P.pop();
-    }
-    return true;
+    }}
+    return valido;
 }
