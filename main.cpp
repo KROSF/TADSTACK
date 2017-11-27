922921ff170f97c048c7cd46e2adb061d4fa83af
rafaroman18 <33625416+rafaroman18@users.noreply.github.com>
2017-11-27 21:07:50 +0100

    Ejercicio 2 Forma Rafa

--- main.cpp
+++ main.cpp
@@ -4,25 +4,67 @@ using namespace std;
 bool shuffle (const string );
 int main()
 {
-    string X="abc&ca";
+    string X="abc&cba#asd&dsa";
     cout<<shuffle(X);
     return 0;
 }
-bool shuffle (const string X){
-    int i;
-    Pila <char>P(X.length());
+bool shuffle (const string X)
+{
+bool valido=true;
+    int i,j,cont,almohadilla;
+    cont=0;
+    Pila<char> P(X.length());
     for(i=0;i<X.length();i++)
     {
-       P.push(X[i]);
-    }
-    for(i=0;i<X.length();i++)
-    {
-        if(P.tope()!=X[i])
+        if(X[i]=='#')
         {
-            cout<<"tu ere tonto"<<endl;
-            return false;
+            cont++;
+            almohadilla = i;
         }
-        P.pop();
     }
-    return true;
+    if(cont==0)
+    {
+        valido= false;
+    }
+    else
+    {
+    for(i=0;i<cont;i++)
+    {
+        string Y;
+        for(j=0;X[j]!='#';i++)
+        {
+            Y[j]=X[j];
+        }
+        Pila<char> Pi(Y.length());
+        for(j=0;Y[i]!='&'&&i<Y.length();j++)
+        {
+            Pi.push(Y[j]);
+        }
+        if(Pi.llena())
+        {
+            valido=false;
+            i=cont;
+        }
+        else
+        {
+            Pi.push('&');
+            for(j=j+1;j<Y.length();j++)
+            {
+                Pi.push(Y[j]);
+            }
+            for(j=0;j<Y.length();j++)
+            {
+                if(Pi.tope()!=Y[j])
+                {
+                    valido=false;
+                    i=cont;
+                }
+                Pi.pop();
+            }
+        }
+    }
+    }
+
+    return valido;
 }
+
