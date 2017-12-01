cd5bf729015e700397fd1c2f386bb834444c7d7b
rafaroman18 <33625416+rafaroman18@users.noreply.github.com>
2017-12-01 19:51:24 +0100

    Ejercicio 2 Forma Rafa, faltan corregir ciertos fallos

--- main.cpp
+++ main.cpp
@@ -1,70 +1,63 @@
 #include <iostream>
-#include "pila.h"
+#include <string>
+#include "piladinamica.h"
 using namespace std;
-bool shuffle (const string );
+bool comprobarXY(string );
+bool shuffle (string );
 int main()
 {
-    string X="abc&cba#asd&dsa";
-    cout<<shuffle(X);
+    string Z="abc&cba#asd&ds";
+    cout<<shuffle(Z);
     return 0;
 }
-bool shuffle (const string X)
-{
-bool valido=true;
-    int i,j,cont,almohadilla;
-    cont=0;
-    Pila<char> P(X.length());
-    for(i=0;i<X.length();i++)
-    {
-        if(X[i]=='#')
-        {
-            cont++;
-            almohadilla = i;
-        }
-    }
-    if(cont==0)
-    {
-        valido= false;
-    }
-    else
-    {
-    for(i=0;i<cont;i++)
-    {
-        string Y;
-        for(j=0;X[j]!='#';i++)
-        {
-            Y[j]=X[j];
-        }
-        Pila<char> Pi(Y.length());
-        for(j=0;Y[i]!='&'&&i<Y.length();j++)
-        {
-            Pi.push(Y[j]);
-        }
-        if(Pi.llena())
-        {
-            valido=false;
-            i=cont;
-        }
-        else
-        {
-            Pi.push('&');
-            for(j=j+1;j<Y.length();j++)
-            {
-                Pi.push(Y[j]);
-            }
-            for(j=0;j<Y.length();j++)
-            {
-                if(Pi.tope()!=Y[j])
-                {
-                    valido=false;
-                    i=cont;
-                }
-                Pi.pop();
-            }
-        }
-    }
-    }
 
+bool shuffle (string Z)
+{
+    long j=0;
+    bool valido=true;
+    while(j<Z.length())
+    {
+        string S;
+        int g=0;
+        while(Z[j]!='#' && j<Z.length())
+        {
+            S= S + Z[j];
+            g++;
+            j++;
+        }
+        if(!comprobarXY(S))
+        {
+            j=Z.length()-1;
+            valido= false;
+        }
+        j++;
+    }
     return valido;
 }
 
+bool comprobarXY(string X)
+{
+    bool valido=true;
+    int i=0;
+    Pila<char> P;
+    while(X[i]!='&')
+    {
+        P.push(X[i]);
+        i++;
+    }
+    if((i>=(X.length()/2)+1)||i<X.length()/2)
+    {
+        valido=false;
+    }
+    else
+    {
+        for (i=i+1; i < X.length(); i++)
+        {
+            if (P.tope() != X[i])
+                valido = false;
+            P.pop();
+        }
+
+    }
+    return valido;
+}
